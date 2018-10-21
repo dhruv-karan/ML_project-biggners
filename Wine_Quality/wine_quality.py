b09173180ad35d1be942626a13d09a40ab418d61
dhruv-karan <dhruvkaran24@gmail.com>
2018-10-21 23:17:00 +0530

    did lots of improvement performed SVM classifier and performed visualition of  significant varaible

--- Wine_Quality/wine_quality.py
+++ Wine_Quality/wine_quality.py
@@ -1,27 +1,25 @@
 # importind neccesary library
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 #importing sata set
 dataset = pd.read_csv('winequality-red.csv')
-X= dataset.iloc[:, 0:11].values
-y = dataset.iloc[:, 11:12].values
+# higher level data analysis
+
+dataset.info()
+dataset.head()
+dataset.describe()
+
+#making varaiable
+X = dataset.drop(['quality'], axis = 1)
+y = dataset['quality']
 
 #making traing and test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 0)
 
-#from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-
-
-# pricting from regression model
-y_pred =  regressor.predict(X_test)
-
-#finding the most signficant factor
-
+#adding 
 import statsmodels.formula.api as sm
 X = np.append(arr=np.ones((1599,1)).astype(int),values=X,axis=1)
 
@@ -41,25 +39,43 @@ X_opt = X[: , [0,2,5,6,7,9,10,11]]
 regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
 regressor_OLS.summary()
 
-# predicting with this 
+#2,5,6,7,9,10,11 are the most significat independent varaible
+# using 2 parameter
+sns.barplot(x='quality', y='volatile acidity',data=dataset)
 
-X_sign = dataset.iloc[:,[2,5,6,7,9,10,11]].values
+# using 5 th parameter
+sns.barplot(x = 'quality', y='chlorides', data = dataset)
 
-from sklearn.cross_validation import train_test_split
-X_sign_train, X_sign_test, y_train, y_test = train_test_split(X_sign, y, test_size=0.2, random_state = 0)
+#6
+sns.barplot(x = 'quality', y='free sulfur dioxide', data = dataset)
+#7
+sns.barplot(x = 'quality', y='total sulfur dioxide', data = dataset)
+#9
+sns.barplot(x = 'quality', y='pH', data = dataset)
 
+#10
+sns.barplot(x = 'quality', y='sulphates', data = dataset)
+#11
+sns.barplot(x = 'quality', y='alcohol', data = dataset)
 
-from sklearn.linear_model import LinearRegression
-regressor1 = LinearRegression()
-regressor1.fit(X_sign_train, y_train)
+# Encoding our dependent variable:Quality column
+from sklearn.preprocessing import LabelEncoder
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
 
-#best prediction 
-y_pred_best =  regressor1.predict(X_sign_test)
+# Feature Scaling to X_train and X_test to classify better.
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+#fitting dataset in claasfier
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
 
+#Predicting the Test Set
+y_pred = classifier.predict(X_test)
 
-# feature 2,5,6,7,9,10,11 are the most significatnt factor in classifing wine quality 
-# 
-
-
-
-
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm,annot=True,fmt='2.0f')
