c9ef9891b180761943c848e6dee91c792d3c850c
dhruv-karan <dhruvkaran24@gmail.com>
2018-10-30 15:43:32 +0530

    house price predicted using SVR

--- boston_housing/housing_price.py
+++ boston_housing/housing_price.py
@@ -6,91 +6,69 @@ import pandas as pd
 
 dataset = pd.read_csv('boston_data.csv')
 X = dataset.iloc[: ,0:14 ].values
-y = dataset.iloc[:,13].values
+y = dataset.iloc[:,13:14].values
+
+dataset.info()
+dataset.head()
+dataset.describe()
 
 #test and trainging sets
 from sklearn.cross_validation import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+#fearure scaling 
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+y_train = sc_y.fit_transform(y_train)
 
-#making model
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
 regressor.fit(X_train,y_train)
 
-y_pred = regressor.predict(X_test)
+
+y_predict = sc_y.inverse_transform(regressor.predict(sc_X.transform(X_test)))
+
+
+
+#print("lets predict price of u r dream house")
+#print("enter the following values ")
+#X_input=[12]
+#X_input[0] = input("crim per capita crime rate by town.")
+#X_input[1] = input("indus proportion of non-retail business acres per town..")
+#X_input[2] = input("chas Charles River dummy variable (= 1 if tract bounds river; 0 otherwise).")
+#X_input[3] = input("nox nitrogen oxides concentration (parts per 10 million)..")
+#X_input[4] = input("rm average number of rooms per dwelling.")
+#X_input[5] = input("age proportion of owner-occupied units built prior to 1940.")
+#X_input[6] = input("dis weighted mean of distances to five Boston employment centres.")
+#X_input[7] = input("rad index of accessibility to radial highways.")
+#X_input[8] = input("tax full-value property-tax rate per $10,000.")
+#X_input[9] = input("ptratio pupil-teacher ratio by town.")
+#X_input[10] = input("black 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town.")
+#X_input[11] = input("lstat lower status of the population (percent).")
+#X_input[12] = input("medv median value of owner-occupied homes in $1000s."
+    
+    
+    
+
+#print(sc_y.inverse_transform(regressor.predict(sc_X.transform([X_input]))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #ploting graph
 #plt.scatter(X_train,y_train,color='red')
-#plt.plot(X_test, regressor.predict(X_test), color='blue')
-
-import statsmodels.formula.api as sm
-X = np.append(arr=np.ones((404,1)).astype(int),values=X,axis=1)
-
-X_opt = X[: , [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[: , [0,1,2,3,4,5,6,7,8,9,10,12,13,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[: , [0,1,2,3,4,5,6,7,8,9,12,13,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-
-X_opt = X[: , [0,1,2,3,4,5,6,7,8,12,13,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[: , [0,1,2,3,4,5,7,8,12,13,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[: , [0,1,2,3,5,7,8,12,13,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-X_opt = X[: , [0,1,2,3,7,8,12,13,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[: , [0,1,2,3,7,12,13,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-
-X_opt = X[: , [0,1,2,3,7,12,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[: , [0,1,3,7,12,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[: , [0,3,7,12,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[: , [0,7,12,14]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-
-
-# hrer we can we that 7 12 14 is the variable with most sergenficance in pref=dicting 
-#house prices
-X_last  = dataset.iloc[:,[7,12,13]].values
-
-from sklearn.cross_validation import train_test_split
-X_last_train,X_last_test,y_last_train,y_last_test = train_test_split(X_last,y,test_size=0.2,random_state=0)
-
-#making model
-from sklearn.linear_model import LinearRegression
-regressor1 = LinearRegression()
-regressor1.fit(X_last_train,y_last_train)
-value  = np.array([[20,4,10]])
-
-y_pred1 = regressor1.predict(value)
-
-
-
 
