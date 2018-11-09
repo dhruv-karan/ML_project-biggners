efc069a8d110c551a400470184bd5d798361e77b
dhruv-karan <dhruvkaran24@gmail.com>
2018-11-09 21:18:26 +0530

    imported datset

---
+++ height-weight/h&w.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  9 21:14:26 2018
+
+@author: dhruv
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('weight-height.csv')
