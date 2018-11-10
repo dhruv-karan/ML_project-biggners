d1dccd8c889f714676a244f9dba9c472b7a10ede
dhruv-karan <dhruvkaran24@gmail.com>
2018-11-10 23:54:33 +0530

    doing

--- height-weight/h&w.py
+++ height-weight/h&w.py
@@ -10,3 +10,35 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('weight-height.csv')
+
+dataset.info()
+dataset.describe()
+
+X = dataset.iloc[:,1:3].values
+y = dataset.iloc[:,0:1].values
+
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+y[:, 0] = labelencoder.fit_transform(y[:, 0])
+
+#making traing and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 0)
+
+
+
+# question1: pred male or female on given data set.
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+y_pred  = y_pred>0.5
+j=0
+for i in y_pred:
+    if(i==True):
+        y_pred[j] = 1
+        j =j+1
+    y_pred[j]=0
+    j = j+1
