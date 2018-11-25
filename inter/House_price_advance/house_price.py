97dc6665ecc0a61684f1a0c9addc3dce95cc3940
dhruv-karan <dhruvkaran24@gmail.com>
2018-11-26 01:56:18 +0530

    half is done

--- inter/House_price_advance/house_price.py
+++ inter/House_price_advance/house_price.py
@@ -15,25 +15,43 @@ corpus = []
 corpus.append(dataset.dtypes)
 corpus.append(dataset.isnull().sum())
 
-dataset.dtypes[42]
+dataset.dtypes[4]
 
 np.array(corpus)
 dataset.dtypes
 dataset.isnull().sum()
-index =dataset.isnull().sum()
+index = list(dataset.isnull().sum())
 np.asarray(index)
-j=-1
-
-total_null=[]
 int_only =[]
-
-for i in index:
-    j+=1
-    if i>0 | (dataset.dtypes[j]!=int):
-        total_null.append(j)
-        
 k=-1     
 for i in index:
     k+=1
     if i>0:
         int_only.append(k)
+X_train= dataset.iloc[:,0:80]
+
+X_train = X_train.drop(['Alley', 'MasVnrType','BsmtQual','BsmtCond','FireplaceQu','PoolQC','BsmtExposure','BsmtFinType1','BsmtFinType2','Electrical','GarageType','GarageFinish','GarageQual','GarageCond','Fence','MiscFeature'],axis=1)
+
+
+index[1]
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values="NaN",strategy="mean",axis=0)
+imputer = imputer.fit(X_train[:, [3,24,49]])
+X_train[:, [3,24,49]] = imputer.transform(X_train[:,[3,24,49]])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X_train[:,[]] = labelencoder_X.fit_transform(X_train[:, 1])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X_train = onehotencoder.fit_transform(X_train).toarray()
+
+p =X_train.dtypes
+dataset[dataset[1]]
+
+mylist = list(X_train.select_dtypes(include=['object']).columns)
+index
+x = -1
+for i in mylist:
+    x+=1
+    if()
