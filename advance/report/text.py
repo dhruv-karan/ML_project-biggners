27bc213e0d38f7495293f7b5f01b85bddf1673e7
dhruv-karan <dhruvkaran24@gmail.com>
2018-12-03 22:44:31 +0530

    extracted the data

--- advance/report/text.py
+++ advance/report/text.py
@@ -3,7 +3,12 @@
 import pandas as pd
 import numpy as np
 from urllib.request import urlopen
-
+import urllib.request
+import csv
+from bs4 import BeautifulSoup
+from time import sleep
+import re
+import nltk
 df  = pd.read_csv('cik_list.csv')
 valid_df = df.iloc[:152,:].values
 ur = valid_df[0][5]
@@ -13,17 +18,94 @@ for i in range(152):
     print(valid_df[0][5])
 
 full_data=[]
-data = pd.read_csv(ur, skiprows=4, header=None, sep='\s+')
-full_data.append(data)
 
 for i in range(152):
     url=valid_df[i][5]
     #print(url)
-    data = pd.read_csv(url)
-    full_data.append(data)
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page,'html.parser')
+    name_box = soup.find
+    full_data.append(name_box) 
 
 
-valid_df[0][5]
+bag_sentence = []
 
-url ='http://www2.conectiv.com/cpd/tps/archives/nj/2017/12/20171205NJA1.txt'
-data = pd.read_csv(url, skiprows=4, header=None, sep='\s+')
+for file in full_data:
+    if(type(file) == 'str'or'object'):
+        print(file)
+        sentence = nltk.sent_tokenize(file)
+        bag_sentence.append(sentence)
+    else:
+        pass
+
+one = full_data[0]
+
+nltk.sent_tokenize(one)
+
+
+nam =  'my name.is dhruv karan'
+sentences = nltk.sent_tokenize(nam)
+
+sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+
+
+j=nam.str.split(pat = '.')
+
+type(one)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+with open('myfile1.txt', 'w+') as f:
+
+    the_text = str(name_box)
+    file = f.write(the_text)
+
+with open('index1.csv', 'a+') as f:
+    mydoc = csv.writer(f)
+    for i in the_text.split('\n'):
+        mydoc.writerow([i])
+
