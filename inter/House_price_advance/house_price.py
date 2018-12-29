b12e62bd3466dbebf5d83ecbe3c51b3f8af756c7
dhruv-karan <dhruvkaran24@gmail.com>
2018-12-29 11:05:19 +0530

    removed the multicolinearty and filtered the high vraince examples

--- inter/House_price_advance/house_price.py
+++ inter/House_price_advance/house_price.py
@@ -57,7 +57,68 @@ sns.heatmap(corrmat, vmax=.8, square=True)
 sns.set()
 cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
 sns.pairplot(df_train[cols], size = 2.5)
-plt.show();
+plt.show()
+
+#missing data
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
+
+#dealing with missing data
+df_train = df_train.drop((missing_data[missing_data['Total'] > 1]).index,1)
+df_train = df_train.drop(df_train.loc[df_train['Electrical'].isnull()].index)
+df_train.isnull().sum().max() #just checking that
+
+#standardizing data
+saleprice_scaled = StandardScaler().fit_transform(df_train['SalePrice'][:,np.newaxis]);
+low_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][:10]
+high_range= saleprice_scaled[saleprice_scaled[:,0].argsort()][-10:]
+print('outer range (low) of the distribution:')
+print(low_range)
+print('\nouter range (high) of the distribution:')
+print(high_range)
+
+
+#bivariate analysis saleprice/grlivarea
+var = 'GrLivArea'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+#the 2 low price points are not representative of the typical case. Therefore, we'll define them as outliers and delete them.
+
+#deleting points
+df_train.sort_values(by = 'GrLivArea', ascending = False)[:2]
+df_train = df_train.drop(df_train[df_train['Id'] == 1299].index)
+df_train = df_train.drop(df_train[df_train['Id'] == 524].index)
+
+#bivariate analysis saleprice/grlivarea
+var = 'TotalBsmtSF'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
+
+
+df_train.sort_values(by = 'TotalBsmtSF', ascending = False)[:3]
+df_train = df_train.drop(df_train[df_train['Id'] == 333].index)
+df_train = df_train.drop(df_train[df_train['Id'] == 497].index)
+df_train = df_train.drop(df_train[df_train['Id'] == 441].index)
+# delete 3 points for same reason
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
