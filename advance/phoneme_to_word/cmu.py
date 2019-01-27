6916b9e2a4741f9d5ee16a3d5266594e82ce0b25
dhruv-karan <dhruvkaran24@gmail.com>
2019-01-27 23:07:10 +0530

    one hot encoder

--- advance/phoneme_to_word/cmu.py
+++ advance/phoneme_to_word/cmu.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 # #read in data
 # prime_dictionary = open('cmudict.dict', 'r')
 # punct_dictionary = open('cmudict.vp', 'r')
@@ -29,3 +29,66 @@ import pandas as pd
 # =============================  UNCOMMENT THE LINES FOR CONVERTING DOWNLOADED FILLES INTO CSV FILE=========================
 
 df = pd.read_csv('cmudict.csv')
+
+df.head(20)
+
+
+# ====================   making char embbeding  dict ====================
+import string
+
+START_PHONE_SYM = '\t'
+END_PHONE_SYM = '\n'
+
+
+def char_list():
+    allowed_symbols = [".", "-", "'"]
+    uppercase_letters = list(string.ascii_uppercase)
+    return [''] + allowed_symbols + uppercase_letters
+
+
+def phone_list():
+    phone_list = [START_PHONE_SYM, END_PHONE_SYM]
+    for line in df['pronunciation']: 
+        phone_list.append(line.strip())
+    return [''] + phone_list
+
+
+def id_mappings_from_list(str_list):
+    str_to_id = {s: i for i, s in enumerate(str_list)} 
+    id_to_str = {i: s for i, s in enumerate(str_list)}
+    return str_to_id, id_to_str
+
+
+# Create character to ID mappings
+char_to_id, id_to_char = id_mappings_from_list(char_list())
+
+# Load phonetic symbols and create ID mappings
+phone_to_id, id_to_phone = id_mappings_from_list(phone_list())
+
+# Example:
+print('Char to id mapping: \n', char_to_id)
+
+
+#============================== converting chacter into one hot vector ======================
+CHAR_TOKEN_COUNT = len(char_to_id)
+PHONE_TOKEN_COUNT = len(phone_to_id)
+
+
+def char_to_1_hot(char):
+    char_id = char_to_id[char]
+    hot_vec = np.zeros((CHAR_TOKEN_COUNT))
+    hot_vec[char_id] = 1.
+    return hot_vec
+
+
+def phone_to_1_hot(phone):
+    phone_id = phone_to_id[phone]
+    hot_vec = np.zeros((PHONE_TOKEN_COUNT))
+    hot_vec[phone_id] = 1.
+    return hot_vec
+
+# Example:
+print('"A" is represented by:\n', char_to_1_hot('A'), '\n-----')
+print('"AH0" is represented by:\n', phone_to_1_hot('AH0'))
+
+
